3832048ddd1ffef5dfdcbed607d4c11d24bc34bf
IndreSaveike <indre.saveike@gmail.com>
2023-05-06 20:30:18 +0300

    add 15uzduotisSeleniumTests

---
+++ 15uzduotis_Seleniu_Test/Driver.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace _15uzduotisSeleniumFramework
+{
+    public class Driver
+    {
+        private static IWebDriver driver = null!;
+
+        public static void InitializeDriver()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public static IWebDriver GetDriver()
+        {
+            return driver;
+        }
+
+        internal static void OpenPage(string url)
+        {
+            driver.Url = url;
+        }
+
+        public static void ShutdownDriver()
+        {
+            driver.Quit();
+        }
+    }
+}
