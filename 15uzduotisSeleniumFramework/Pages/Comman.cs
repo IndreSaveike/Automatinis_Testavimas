16d7236a25fd06462c3c30c0060c68f2b9078cc6
IndreSaveike <indre.saveike@gmail.com>
2023-05-06 21:16:22 +0300

    Add 15uzduotisSeleniumTest

---
+++ 15uzduotisSeleniumFramework/Pages/Comman.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using _15uzduotisSeleniumFramework;
+
+namespace _15uzduotisSeleniumFramework.Pages
+{
+    public class Common
+    {
+        private static IWebElement GetElement(string locator)
+        {
+            return Driver.GetDriver().FindElement(By.XPath(locator));
+        }
+
+        public static void Click(string locator)
+        {
+            GetElement(locator).Click();
+        }
+
+        public static void SendKeys(string locator, string keys)
+        {
+            GetElement(locator).SendKeys(keys);
+        }
+
+        public static string GetElementText(string locator)
+        {
+            return GetElement(locator).Text;
+        }
+    }
+}
---
+++ 15uzduotisSeleniumFramework/Pages/SeleniumEasy/DemoqaTextBox.cs
@@ -0,0 +1,71 @@
+﻿using _15uzduotisSeleniumFramework;
+using _15uzduotisSeleniumFramework.Pages;
+using OpenQA.Selenium;
+using System;
+
+namespace _15uzduotisSeleniumFramework.Pages.SeleniumEasy
+{
+    public class DemoqaTextBox
+    {
+
+        public static void Open()
+        {
+            Driver.OpenPage("https://demoqa.com/text-box");
+        }
+
+        public static void EnterUserName(string userName)
+        {
+            string locator = "//*[@id='userName']";
+            Common.SendKeys(locator, userName);
+        }
+
+        public static void EnterUserEmail(string userEmail)
+        {
+            string locator = "//*[@id='userEmail']";
+            Common.SendKeys(locator, userEmail);
+        }
+
+        public static void EnterCurrentAddress(string currentAddress)
+        {
+            string locator = "//*[@id='currentAddress']";
+            Common.SendKeys(locator, currentAddress);
+        }
+
+        public static void EnterPermanentAddress(string permanentAddress)
+        {
+            string locator = "//*[@id='permanentAddress']";
+            Common.SendKeys(locator, permanentAddress);
+        }
+
+        public static void ClickSubmit()
+        {
+            string locator = "//*[@id='submit']";
+            Common.Click(locator);
+        }
+
+        public static string GetNameText()
+        {
+            string locator = "//*[@id='name']";
+            return Common.GetElementText(locator);
+        }
+
+        public static string GetEmailText()
+        {
+            string locator = "//*[@id='email']";
+            return Common.GetElementText(locator);
+        }
+
+        public static string GetCurrentAddressText()
+        {
+            string locator = "//*[@id='output']//*[contains(@id,'currentAddress')]";
+            return Common.GetElementText(locator);
+        }
+
+        public static string GetPermanentAddressText()
+        {
+            string locator = "//*[@id='output']//*[contains(@id,'permanentAddress')]";
+            return Common.GetElementText(locator);
+        }
+    }
+}
+
